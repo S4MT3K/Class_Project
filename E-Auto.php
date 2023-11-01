46407c0846b93ddd80d37b7352604fb5302aa1b0
S4MT3K <3rdlvl@gmx.de>
2023-11-01 08:20:22 +0100

    Pulled From Manuel Version 2.0 VMAX Correction.

---
+++ E-Auto.php
@@ -0,0 +1,79 @@
+<?php
+
+class ECar
+{
+    private static self $Ecar ;                                                                              //auf null gesetzt weil die erste "(nicht) instanz" null ist also nichts weil nicht erstellt
+
+    private string $brand;
+    private string $model;
+    private int    $year;
+    private int    $range   = 600; //km
+    public  int    $loading = 100; //%
+
+
+    private function __construct($Brand, $Model, $year, $range, $loading)
+    {
+        $this->brand = $Brand;
+        $this->model = $Model;
+        $this->year = $year;
+        $this->range = $range;
+        $this->loading = $loading;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public static function ECarInit($brnd, $mdl, $yr, $rng, $ld) : self
+    {
+
+        self::$Ecar = new self($brnd, $mdl, $yr, $rng, $ld);
+        return self::$Ecar;
+    }
+
+    public function getBatteryLevel() : string    {
+        return "Die Batterieladung beträgt ungefähr $this->loading%";
+    }
+
+    public function LoadBattery($amountPercentage) : void
+    {
+        $this->loading = $amountPercentage;
+        echo "Loading Battery to $amountPercentage %";
+    }
+
+    public function SetBatteryLevel($amountPercentage) : void
+    {
+        $this->loading = $amountPercentage;
+    }
+
+    public function getrange() : string
+    {
+        $rangeLeft = ($this->loading / 100) * $this->range;
+        return "Die Restreichweite bei $this->loading% ladung beträgt ungefähr $rangeLeft km";
+    }
+}
+
+
+$vw = ECar::ECarInit('Volkswagen', 'ID3', 2019, 600, 100);
+$BMW = ECar::ECarInit('BMW', '320D', 2012, 1200, 100); //VERERBEN WEIL IST DIESEL
+
+echo $vw->getBatteryLevel();
+echo '<br>';
+echo $vw->getrange();
+echo '<br>';
+echo $vw->SetBatteryLevel(60);
+echo '<br>';
+echo $vw->getBatteryLevel();
+echo '<br>';
+echo $vw->getrange();
+echo '<br>';
+echo $vw->LoadBattery(100);
+echo '<br>';
+echo $vw->getrange();
+echo '<br>';
+echo '<br>';
+
+echo $vw->getModel();
+echo '<br>';
+echo $BMW->getModel();
--- Mensch.php
+++ Mensch.php
@@ -2,28 +2,76 @@
 //Erstelle eine Klasse "Auto" mit den Attributen public "Marke", "Modell" und "Baujahr". Erstelle dann zwei Objekte dieser Klasse und setze die Werte für diese Attribute.
 
 
-class Auto
+class Auto //Version in der immer nur eine instanz erstellt wird, immer und immer wieder
 {
+    private static int $anzahl = 0;
+    private static self|NULL $auto = null;
+    //-----
+
     private string $marke;
     private string $modell;
     private int $baujahr;
     private int $kmstand;
     private int $vmax;
-    private string $nickname;
+    private string|null $nickname;
 
-    public function __construct($Marke, $Modell, $Baujahr, $nickname,$kmstand,$vmax )
+
+    private function __construct($Marke, $Modell, $Baujahr, $kmstand, $vmax, $nickname = null)
     {
         $this->marke = $Marke;
         $this->modell = $Modell;
         $this->baujahr = $Baujahr;
-        $this->nickname = $nickname;
         $this->kmstand = $kmstand;
         $this->vmax = $vmax;
+        $this->nickname = $nickname;
+        self::$anzahl++;
     }
 
-    public function set_marke($marke): void
+    public static function makeCar($Brand, $Mod, $year, $km, $vmax, $nickname = null)
     {
-        $this->marke = $marke;
+        if(self::$auto === null)
+        {
+            self::$auto = new self($Brand, $Mod, $year, $km, $vmax, $nickname);
+        }
+        return self::$auto;
+    }
+
+    public function getInfo() : string
+    {
+    return
+        '<div class="container col s4" >
+        <div class="center green" style="border-radius: 12px; box-shadow: 5px 5px rgba(0,50,0, 0.4); height: 50px; width: 80%; line-height: 3.25rem"> Manufactor: ' . "$this->marke" . '</div>
+        <p></p>
+        <div class="center green" style="border-radius: 12px; box-shadow: 5px 5px rgba(0,50,0, 0.4); height: 50px; width: 80%; line-height: 3.25rem"> Model: ' . "$this->modell" . '</div>
+        <p></p><div class="center green" style="border-radius: 12px; box-shadow: 5px 5px rgba(0,50,0, 0.4); height: 50px; width: 80%; line-height: 3.25rem"> Year: ' . "$this->baujahr" . '</div>
+        <p></p><div class="center green" style="border-radius: 12px; box-shadow: 5px 5px rgba(0,50,0, 0.4); height: 50px; width: 80%; line-height: 3.25rem"> Mileage: ' . "$this->kmstand" . '</div>
+        <p></p><div class="center green" style="border-radius: 12px; box-shadow: 5px 5px rgba(0,50,0, 0.4); height: 50px; width: 80%; line-height: 3.25rem"> Vmax: ' . "$this->vmax" . '</div>' . "{$this->get_nickname()}" .
+        '</div>';
+    }
+
+    public function get_manufactor(): string
+    {
+        return $this->marke;
+    }
+
+    public function get_model(): string
+    {
+        return $this->modell;
+    }
+
+    public function get_year(): string
+    {
+        return $this->baujahr;
+    }
+
+    public function get_Mileage(): string
+    {
+        return $this->kmstand;
+    }
+
+    public function get_Vmax(): string
+    {
+        return $this->vmax;
     }
 
     public function set_nickname($nick)
@@ -35,27 +83,55 @@ class Auto
     {
         if (empty($this->nickname))
         {
-            return 'Dieses Fahrzeug hat keinen Nicknamen!';
+            return "$this->nickname";
         }
-        return "Dieses Fahrzeug heisst $this->nickname";
+        return '<p></p><div class="center green" style="border-radius: 12px; box-shadow: 5px 5px rgba(0,50,0, 0.4); height: 50px; width: 80%; line-height: 3.25rem"> Nickname: ' . "$this->nickname" . '</div>';
     }
 }
+?>
 
-$VwGTI = new Auto('Volkswagen', 'GTI', '2017', "", 122400, 260);
-$BMW320D = new Auto('Bayerische Motoren Werke', '320D', '2010', "Bimi", 260440, 260);
-//$VwGTI->marke = 'DAAAAS AUTO';
-echo '<pre>';
-print_r($VwGTI);
-echo '</pre>';
+<!doctype html>
+<?php //Initialize Cars
 
-echo '<pre>';
-print_r($BMW320D);
-echo '</pre>';
+//---------
+$VolksWagen = Auto::makeCar('Volkswagen', 'GTI', 2014, 76231, 265, 'Dicki');
+$BMW = Auto::makeCar('BMW', '320D', 2012, 26314, 260, 'BIMI');
+$Fiat = Auto::makeCar('FIAT', 'Punto', 2000, 176391, 110);
+//---------
+$MyCars = [$VolksWagen, $BMW, $Fiat];
+//$BMW = new Auto('BMW', '320D', 2012, 'Bimi', 266312, 260); ?>
+<html lang="de" xmlns="http://www.w3.org/1999/html">
+<head>
+    <style>
+        body {
+            background:
+                /* top, transparent black, faked with gradient */
+                    linear-gradient(
+                            rgba(0, 0, 0, 0.7),
+                            rgba(0, 0, 0, 0.7)
+                    ),
+                        /* bottom, image */
+                    url(images/CarShow-006.png);
+            background-size: cover;
+        }
+    </style>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Car Und So</title>
+</head>
+<body>
+<div class="center red" style="border-radius: 12px; box-shadow: 5px 5px rgba(0,0,0, 0.2)"> <h1 style="margin-top: 10px;"> GET YOUR CAR IDENTITY </h1> </div>
 
-echo '<pre>';
-echo $VwGTI->get_nickname();
-echo '<br>';
-echo $BMW320D->get_nickname();
-echo '</pre>';
-
-
+<div class="row">
+<?php
+foreach ($MyCars as $auto) {
+    echo $auto -> getInfo();
+}
+?>
+</div>
+</body>
+</html>
